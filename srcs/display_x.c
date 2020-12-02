06be6a0fffb2635e97a0436cf1851cba3d7b3d93
Augustin Debureaux <adebureaux@gmail.com>
2020-12-02 03:49:31 +0100

    Secure wrong specifier

--- srcs/display_x.c
+++ srcs/display_x.c
@@ -6,7 +6,7 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/29 16:12:29 by adeburea          #+#    #+#             */
-/*   Updated: 2020/12/01 23:08:56 by adeburea         ###   ########.fr       */
+/*   Updated: 2020/12/02 03:16:33 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	display_x(t_ptf *ptf, unsigned int nb)
 {
-	char			base[17];
+	char base[17];
 
 	if (ptf->type == 'X')
 		ft_strlcpy(base, "0123456789ABCDEF", 17);
--- srcs/ft_printf.c
+++ srcs/ft_printf.c
@@ -6,31 +6,28 @@
 /*   By: adeburea <adeburea@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 17:40:36 by adeburea          #+#    #+#             */
-/*   Updated: 2020/12/01 23:13:31 by adeburea         ###   ########.fr       */
+/*   Updated: 2020/12/02 03:47:49 by adeburea         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/ft_printf.h"
 
-void	display_type(t_ptf *ptf)
+void		display_type(t_ptf *ptf)
 {
-	if (ft_strchr(ptf->spec, ptf->type))
-	{
-			if (ptf->type == 'c')
-				display_c(ptf);
-			if (ptf->type == 'c')
-				display_s(ptf);
-			if (ptf->type == 'p')
-				display_p(ptf);
-			if (ptf->type == 'd' || ptf->type == 'i')
-				display_d(ptf);
-			if (ptf->type == 'u')
-				display_u(ptf);
-			if (ptf->type == 'x' || ptf->type == 'X')
-				display_x(ptf, va_arg(ptf->vl, unsigned));
-			if (ptf->type == '%')
-				ptf->ret += ft_putchar('%');
-	}
+	if (ptf->type == 'c')
+		display_c(ptf);
+	if (ptf->type == 's')
+		display_s(ptf);
+	if (ptf->type == 'p')
+		display_p(ptf);
+	if (ptf->type == 'd' || ptf->type == 'i')
+		display_d(ptf);
+	if (ptf->type == 'u')
+		display_u(ptf);
+	if (ptf->type == 'x' || ptf->type == 'X')
+		display_x(ptf, va_arg(ptf->vl, unsigned));
+	if (ptf->type == '%')
+		ptf->ret += ft_putchar('%');
 }
 
 char	*parse_spec(t_ptf *ptf, char *str)
@@ -50,13 +47,15 @@ char	*parse_spec(t_ptf *ptf, char *str)
 		i += int_len((ptf->width = ft_atoi(&str[i])), 1);
 	if (str[i] == '.')
 	{
-		if (str[i++] == '*')
+		if (str[++i] == '*')
 			i += int_len((ptf->prec = va_arg(ptf->vl, int)), 1);
 		else if (ft_isdigit(str[i]))
 			i += int_len((ptf->prec = ft_atoi(&str[i])), 1);
 	}
-	ptf->type = ft_strchr(ptf->spec, str[i])[0];
-	//display_ptf(ptf);
+	ptf->type = str[i];
+	display_ptf(ptf);
+	if (!ptf->type || !ft_strchr(ptf->spec, ptf->type))
+		return (NULL);
 	display_type(ptf);
 	return (&str[i]);
 }
@@ -65,7 +64,8 @@ t_ptf	*init_ptf(void)
 {
 	t_ptf	*ptf;
 
-	ptf = (t_ptf*)malloc(sizeof(t_ptf));
+	if (!(ptf = (t_ptf*)malloc(sizeof(t_ptf))))
+		return (NULL);
 	ft_strlcpy(ptf->spec, "cspdiuxX%", 10);
 	ft_strlcpy(ptf->flag, "-0.*", 5);
 	ptf->ret = 0;
@@ -82,12 +82,16 @@ int		ft_printf(const char *str, ...)
 {
 	t_ptf	*ptf;
 
-	ptf = init_ptf();
+	if (!(ptf = init_ptf()))
+		return (-1);
 	va_start(ptf->vl, str);
 	while (*str)
 	{
 		if (*str == '%' && *str + 1)
-			str = parse_spec(ptf, (char*)++str);
+		{
+			if (!(str = parse_spec(ptf, (char*)++str)))
+				return (-1);
+		}
 		else
 			ptf->ret += ft_putchar(*str);
 		str++;
